6f9d953b5dc882ad049ced88696b89cbf521042a
lifestreamy <timkorelov@gmail.com>
2025-06-23 23:57:41 +0300

    Replace the maven publish plugin with 3-rd party one to test next and maybe keep

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     id("org.jetbrains.dokka") version "2.0.0"
-    `maven-publish`
+//    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.33.0"
 }
 
 
@@ -13,13 +14,13 @@ kotlin {
 
 }
 
-publishing {
-    repositories {
-        maven {
-            // todo later
-        }
-    }
-}
+//publishing {
+//    repositories {
+//        maven {
+//
+//        }
+//    }
+//}
 
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
