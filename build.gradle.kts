7e86a010ed12041311e56503bfc55a8ac44a86cc
lifestreamy <timkorelov@gmail.com>
2025-06-24 23:17:43 +0300

    Set up build.gradle for maven publishing and Dokka docs
    Update versions, add line to gradle.properties to enable Dokka V2 as per Jetbrains' suggestion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,65 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.1.10"
     id("org.jetbrains.dokka") version "2.0.0"
-//    `maven-publish`
     id("com.vanniktech.maven.publish") version "0.33.0"
 }
 
+group = "com.github.lifestreamy.treebuilder"
+version = "2.0.2"
 
 kotlin {
-    group = "com.github"
-    version = "2.0.1"
-
     jvmToolchain(17)
-
 }
 
-//publishing {
-//    repositories {
-//        maven {
-//
-//        }
-//    }
-//}
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
 
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
+    coordinates(group.toString(), "treebuilder", version.toString())
+
+    pom {
+        name.set("TreeBuilder")
+        description.set("Kotlin library for building tree structures")
+        url.set("https://github.com/lifestreamy/TreeBuilder")
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://opensource.org/licenses/Apache-2.0")
+            }
+        }
+        developers {
+            developer {
+                id.set("lifestreamy")
+                name.set("Tim K.")
+            }
+        }
+        scm {
+            url.set("https://github.com/lifestreamy/TreeBuilder")
+            connection.set("scm:git:git://github.com/lifestreamy/TreeBuilder.git")
+        }
+    }
+}
+
+tasks.dokkaHtml {
+    outputDirectory.set(layout.buildDirectory.dir("dokka").get().asFile)
+}
+
+tasks.dokkaJavadoc.configure {
+    outputDirectory.set(layout.buildDirectory.dir("dokkaJavadoc").get().asFile)
+}
+
+val dokkaJar by tasks.register<Jar>("dokkaJar") {
+    dependsOn(tasks.dokkaJavadoc)
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaJavadoc.get().outputDirectory)
+}
+
+val sourcesJar by tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+tasks.test {
     useJUnitPlatform()
 }
 
